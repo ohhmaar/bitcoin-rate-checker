c6c4f8fda703da58c516ef9afaedbca7dc46eacf
Omar Basrawi <Omar@Omars-MacBook-Air.local>
2015-05-17 22:33:39 -0500

    Initial Commit

--- Bitcoin/ViewController.m
+++ Bitcoin/ViewController.m
@@ -9,15 +9,21 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *priceLabel;
+@property (strong, nonatomic) IBOutlet UILabel *countdownLabel;
 
 @end
 
 @implementation ViewController
+int counter;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self getPrice];
+    counter = 10;
+    [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(countdown) userInfo:nil repeats:YES];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +32,27 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+-(void)getPrice{
+    NSURL *url = [NSURL URLWithString:@"https://coinbase.com/api/v1/prices/spot_rate"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError)
+    {
+        if (connectionError == nil) {
+            NSDictionary *spotPrice = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
+            
+            self.priceLabel.text = [[[spotPrice objectForKey:@"amount"] stringByAppendingString:@" "] stringByAppendingString:[spotPrice objectForKey:@"currency"]];
+            
+        }
+    }];
+}
+
+-(void)countdown {
+    counter--;
+    self.countdownLabel.text = [NSString stringWithFormat:@"%d", counter];
+    if (counter == 0) {
+        counter = 15;
+        [self getPrice];
+    }
+}
 @end
